552acd3d5338e1bede05dba2b61d714ef2ff6739
Achiraya Pongcharoen <achiraya.pon@ku.th>
2023-12-07 03:04:52 +0700

    solition

--- bag-exercise/exercise/bag.py
+++ bag-exercise/exercise/bag.py
@@ -8,10 +8,9 @@ Department of Computer Engineering, Kasetsart University
 MIT License
 
 """
-
-# These two lines add type-union support and create a real-number type.
 from typing import Union
 RealType = Union[int, float]
+
 
 class Bag:
     """
@@ -36,6 +35,8 @@ class Bag:
         >>> Bag().bagweight
         0
         """
+        self.bagweight = bagweight
+        self.item_dict = {}
 
     def add(self, itemname: str, itemweight: RealType) -> None:
         """ Adds a new item to the Bag. The function will do nothing if there
@@ -56,7 +57,14 @@ class Bag:
             >>> b.add("sTick", 1.1)
             >>> b.count()
             2
+
         """
+        if itemname.lower() not in ['bag', 'a bag']:  # check bag
+            if 0 <= itemweight <= 10000:  # check item weight
+                if all(ord(c) < 128 for c in itemname):  # check emoji
+                    if itemname not in self.item_dict:
+                        self.item_dict[itemname] = itemweight
+                        self.bagweight += itemweight
 
     def remove(self, itemname: str) -> tuple[str, RealType]:
         """ Removes the item identified by itemname from the Bag and
@@ -66,7 +74,15 @@ class Bag:
             >>> b.add("stone", 10)
             >>> b.remove("stone")
             ('stone', 10)
+
         """
+        if len(self.item_dict) != 0 and itemname in self.item_dict:
+            new_tuple = (itemname, self.item_dict[itemname])
+            self.bagweight -= self.item_dict[itemname]
+            del self.item_dict[itemname]
+
+            return new_tuple
+        return None
 
     def weight(self) -> RealType:
         """ Returns the total (gross) weight.
@@ -81,6 +97,7 @@ class Bag:
             >>> b.weight()
             32.9
         """
+        return self.bagweight
 
     def items(self) -> list[str]:
         """ Returns the list of all item names in the Bag.
@@ -93,6 +110,7 @@ class Bag:
             >>> sorted(b.items())
             ['cheese', 'egg', 'potion']
         """
+        return self.item_dict.keys()
 
     def dump(self) -> list[tuple[str, RealType]]:
         """ Completely empties the Bag and returns all items as a list of tuples
@@ -103,6 +121,14 @@ class Bag:
             >>> b.dump()
             [('potion', 2)]
         """
+        new_list = []
+        for item, weight in self.item_dict.items():
+            new_list.append((item, weight))
+
+        self.item_dict = {}
+        self.bagweight = 0
+
+        return new_list
 
     def count(self) -> int:
         """ Returns the number (int) of items inside the bag.
@@ -117,16 +143,19 @@ class Bag:
             >>> b.count()
             2
         """
+        return len(self.item_dict)
 
     def addmany(self, itemlist: list[tuple[str, RealType]]):
         """ Similar to add, but many items at a time. The itemlist variable is a
-            list of tuples [(itemname, itemweight), ...].
+            list of tuples [(itemname, itemweight), ...]
 
             >>> b = Bag()
-            >>> b.addmany([("a", 1), ("b", 2)])
+            >>> b.addmany([("a", 1), ("b", 2))
             >>> b.count()
             2
         """
+        for tuple_in_list in itemlist:
+            self.add(tuple_in_list[0], tuple_in_list[1])
 
     def removemany(self, namelist: list[str]) -> list[tuple[str, RealType]]:
         """ Similar to remove, but the argument is a list of names
@@ -139,11 +168,18 @@ class Bag:
             >>> sorted(b.removemany(["b", "a"]))
             [('a', 1), ('b', 2)]
         """
+        # if len(self.item_dict) != 0 and :
+        new_list = []
+        for name in namelist:
+            new_list.append((name, self.item_dict[name]))
+            self.remove(name)
+
+        return new_list
+        # return None
 
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
     # Use the following line INSTEAD if you want to print all tests anyway.
-    # doctest.testmod(verbose = True)
-
+    doctest.testmod(verbose=True)
